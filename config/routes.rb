9ca9289ce75047733dbd5accaa61b36651339804
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-09-27 20:24:55 +0200

    remove daisyui, contact route and write hero section of home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/contatti", to: "submissions#new"
   get "/affiliati", to: "affiliate_products#index"
 
   resources :submissions, only: [ :new, :create ]
