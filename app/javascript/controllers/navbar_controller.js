2214c497cc391e354f3081ed0d13c0d0a30e5da7
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2025-10-06 10:46:13 +0200

    create navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,7 +1,38 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="navbar"
 export default class extends Controller {
+  static targets = ["nav"]
+
   connect() {
+    this.scrollThreshold = 100
+    this.isVisible = false
+    this.handleScroll = this.handleScroll.bind(this)
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  disconnect() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll() {
+    const scrollY = window.scrollY
+
+    if (scrollY > this.scrollThreshold && !this.isVisible) {
+      this.show()
+    } else if (scrollY <= this.scrollThreshold && this.isVisible) {
+      this.hide()
+    }
+  }
+
+  show() {
+    this.isVisible = true
+    this.navTarget.classList.remove("-translate-y-full")
+    this.navTarget.classList.add("translate-y-0")
+  }
+
+  hide() {
+    this.isVisible = false
+    this.navTarget.classList.remove("translate-y-0")
+    this.navTarget.classList.add("-translate-y-full")
   }
 }
